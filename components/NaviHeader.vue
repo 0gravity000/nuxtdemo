0b0d7dd34acc7939ec9616de7117f8230da69071
0gravity000 <0gravity000@gmail.com>
2021-08-30 01:08:29 +0900

    Add layout default.vue

---
+++ components/NaviHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="row">
+    <div class="col">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <NuxtLink to="/">Welcome</NuxtLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <NaviHeader />
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <Nuxt />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
