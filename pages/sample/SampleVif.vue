d5d21221d7b3f3242af2d21134b31a19c3fca9ef
0gravity000 <0gravity000@gmail.com>
2021-09-04 20:33:25 +0900

    v-ifページを編集（key属性による再利用可能な要素の制御を追加）

--- pages/sample/SampleVif.vue
+++ pages/sample/SampleVif.vue
@@ -25,6 +25,10 @@
       Not A/B/C
     </div>
 
+    <h4>key属性による再利用可能な要素の制御</h4>
+    <a :href="pagelink">
+      リンク
+    </a>
     <h4>v-if vs v-show</h4>
     <p>一般的に、v-if はより高い切り替えコストを持っているのに対して、 v-show はより高い初期描画コストを持っています。 </p>
     <p>とても頻繁に何かを切り替える必要があれば v-show を選び、条件が実行時に変更することがほとんどない場合は、v-if を選びます。</p>
@@ -37,7 +41,8 @@ export default {
   data () {
     return {
       seen: true,
-      type: 'A'
+      type: 'A',
+      pagelink: 'https://jp.vuejs.org/v2/guide/conditional.html#key-%E3%81%AB%E3%82%88%E3%82%8B%E5%86%8D%E5%88%A9%E7%94%A8%E5%8F%AF%E8%83%BD%E3%81%AA%E8%A6%81%E7%B4%A0%E3%81%AE%E5%88%B6%E5%BE%A1'
     }
   },
   methods: {
