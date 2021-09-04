a872d45b9cf0049a6c676c2b1ca12eee8d8a5828
0gravity000 <0gravity000@gmail.com>
2021-09-04 15:31:13 +0900

    マスタッシュ構文、vｰbindページの編集、nuxtdemo.cssの追加

--- pages/sample/mustache.vue
+++ pages/sample/mustache.vue
@@ -1,7 +1,22 @@
 <template>
   <div>
-    <h3>マスタッシュ構文{{}}</h3>
+    <h3>マスタッシュ構文{{}} Mustache構文(二重中括弧)</h3>
+    <p>2重中括弧の mustaches は、データを HTML ではなく、プレーンなテキストとして扱います。</p>
     <p>{{ message }}</p>
+    <h4>v-once ディレクティブ</h4>
+    <p>v-once ディレクティブを使用することで、データ変更時の更新はおこなわず、一度だけ展開することができます。</p>
+    <p>ただし、同じノードのあらゆる他のバインディングが影響を受けることに注意してください</p>
+    <span v-once>v-onceは、変化しない: {{ once_message }}</span>
+    <h4>生のHTML v-html ディレクティブ</h4>
+    <p>実際の HTML として出力するためには、v-html ディレクティブを使用する必要があります</p>
+    <p>Using mustaches: {{ rawHtml }}</p>
+    <p>Using v-html directive: <span v-html="rawHtml" /></p>
+    <p>XSS 脆弱性を容易に引き起こすので、ウェブサイトで動的に任意のHTMLを描画することは、非常に危険です。</p>
+    <p>信頼できるコンテンツにだけ HTML 展開を利用してください。ユーザーから提供されたコンテンツに対しては決して使用してはいけません。</p>
+    <h4>JavaScript 式の使用</h4>
+    <p>JavaScript 式を完全にサポートする。文は動作しない。</p>
+    <p>テンプレート式はサンドボックスで、Math や Date といった ホワイトリストにあるグローバルオブジェクト だけにアクセスできます。</p>
+    <p>テンプレート式内でユーザーが定義したグローバルオブジェクトにアクセスしようとしてはいけません。</p>
   </div>
 </template>
 
@@ -10,7 +25,9 @@ export default {
   layout: 'sample',
   data () {
     return {
-      message: 'これはメッセージ'
+      message: 'これはメッセージ',
+      once_message: 'これはv-onceのメッセージ',
+      rawHtml: '<span style="color:red">これは赤色</span>'
     }
   }
 }
--- pages/sample/vbind.vue
+++ pages/sample/vbind.vue
@@ -4,6 +4,16 @@
     <span :title="message">
       マウスを数秒間、この文章の上に置くと、動的にバインドされたタイトルが表示されます。
     </span>
+    <p>Mustache は、HTML 属性の内部で使用することはできません。代わりに、v-bind ディレクティブを使用する</p>
+    <div :id="dynamicId" />
+    <h4>属性が真偽値属性の場合</h4>
+    <p>isButtonDisabled が null、undefined、または false の値を持つ場合、disabled属性は描画された button要素に含められません。</p>
+    <button :disabled="isButtonDisabled">
+      Button
+    </button>
+    <h4>動的引数 バージョン 2.6.0 から</h4>
+    <p>角括弧で囲むことで JavaScript 式をディレクティブの引数に使うこともできます</p>
+    <a :[attributeName]="url">リンク</a>
   </div>
 </template>
 
@@ -12,7 +22,9 @@ export default {
   layout: 'sample',
   data () {
     return {
-      message: 'title属性にバインドされたメッセージ'
+      message: 'title属性にバインドされたメッセージ',
+      attributeName: 'href',
+      url: 'https://jp.vuejs.org/v2/guide/syntax.html#%E5%8B%95%E7%9A%84%E5%BC%95%E6%95%B0'
     }
   }
 }
